51defe08c8a4c163ae7b1ce3c45dcaa88d7f62cf
OldBigBuddha <development@oldbigbuddha.net>
2019-02-07 21:36:23 +0200

    Base of project

---
+++ app/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("android.extensions")
+}
+
+android {
+    compileSdkVersion(28)
+    defaultConfig {
+        applicationId = "net.oldbigbuddha.hellokts"
+        minSdkVersion(21)
+        targetSdkVersion(28)
+        versionCode = 1
+        versionName = "1.0.0"
+        testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
+
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.21")
+    implementation("androidx.appcompat:appcompat:1.1.0-alpha01")
+    implementation("androidx.core:core-ktx:1.1.0-alpha04")
+    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+    implementation("com.google.android.material:material:1.1.0-alpha03")
+    testImplementation("junit:junit:4.12")
+    androidTestImplementation("androidx.test:runner:1.1.2-alpha01")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.1.2-alpha01")
+}
---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+buildscript {
+    repositories {
+        google()
+        jcenter()
+        
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:3.3.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.21")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+    }
+}
+
+tasks.register("clean", Delete::class.java) {
+    delete(rootProject.buildDir)
+}
